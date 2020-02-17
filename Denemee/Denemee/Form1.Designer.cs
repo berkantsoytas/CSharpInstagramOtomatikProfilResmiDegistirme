7dc2adb7b1eabb9cf5e32a8c4e0289339e58ddcd
berkantsoytas <54627016+berkantsoytas@users.noreply.github.com>
2020-02-17 23:17:53 +0300

    Uygulama Eklendi Direkt

---
+++ Denemee/Denemee/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Denemee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDogrulama = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pbPP = new System.Windows.Forms.PictureBox();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.txtParola = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPP)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(345, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(36, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "ok";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtDogrulama
+            // 
+            this.txtDogrulama.Location = new System.Drawing.Point(154, 186);
+            this.txtDogrulama.Name = "txtDogrulama";
+            this.txtDogrulama.PasswordChar = '*';
+            this.txtDogrulama.Size = new System.Drawing.Size(185, 20);
+            this.txtDogrulama.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Doğrulama Kodu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label5.Location = new System.Drawing.Point(241, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(164, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Twitter : berkantsoytas";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Purple;
+            this.label4.Location = new System.Drawing.Point(109, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "İG : berkant.exe";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label3.Location = new System.Drawing.Point(10, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Fb: bsbotx";
+            // 
+            // pbPP
+            // 
+            this.pbPP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbPP.Location = new System.Drawing.Point(294, 14);
+            this.pbPP.Name = "pbPP";
+            this.pbPP.Size = new System.Drawing.Size(108, 108);
+            this.pbPP.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbPP.TabIndex = 17;
+            this.pbPP.TabStop = false;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBaslat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBaslat.ForeColor = System.Drawing.Color.Green;
+            this.btnBaslat.Location = new System.Drawing.Point(10, 72);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(278, 50);
+            this.btnBaslat.TabIndex = 16;
+            this.btnBaslat.Text = "Giriş yap ve İşlemi başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.BtnBaslat_Click);
+            // 
+            // txtParola
+            // 
+            this.txtParola.Location = new System.Drawing.Point(103, 43);
+            this.txtParola.Name = "txtParola";
+            this.txtParola.PasswordChar = '*';
+            this.txtParola.Size = new System.Drawing.Size(185, 20);
+            this.txtParola.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Parola:";
+            // 
+            // txtKullaniciAdi
+            // 
+            this.txtKullaniciAdi.Location = new System.Drawing.Point(103, 14);
+            this.txtKullaniciAdi.Name = "txtKullaniciAdi";
+            this.txtKullaniciAdi.Size = new System.Drawing.Size(185, 20);
+            this.txtKullaniciAdi.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 161);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtDogrulama);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbPP);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.txtParola);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtKullaniciAdi);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Instagram Saat Profil Fotoğrafı by berkant.exe";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPP)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDogrulama;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbPP;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.TextBox txtParola;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKullaniciAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Denemee/Denemee/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InstagramApiSharp;
+using InstagramApiSharp.API.Builder;
+using InstagramApiSharp.API;
+using InstagramApiSharp.Classes;
+using InstagramApiSharp.Enums;
+using InstagramApiSharp.Helpers;
+using InstagramApiSharp.Logger;
+using System.IO;
+
+namespace Denemee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static IInstaApi api;
+        private static UserSessionData user;
+        private string zaman = "";
+        private Bitmap bmp;
+        private Bitmap bmp1;
+        private Bitmap bmp2;
+        private Bitmap bmp3;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Dogrula(api);
+        }
+
+        private async void BtnBaslat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtKullaniciAdi.Text))
+                {
+                    MessageBox.Show("Kullanıcı Adı Boş Olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(txtParola.Text))
+                {
+                    MessageBox.Show("Parola Boş Olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                UserSessionData userSessionData = new UserSessionData();
+                user = userSessionData;
+                user.UserName = txtKullaniciAdi.Text;
+                user.Password = txtParola.Text;
+                api = InstaApiBuilder.CreateBuilder().SetUser(user).UseLogger(new DebugLogger(LogLevel.Exceptions)).Build();
+                
+
+                var logInResult = await api.LoginAsync();
+                if (logInResult.Succeeded)
+                {
+                    timer1.Start();
+                }
+                else
+                {
+                    if (logInResult.Value == InstaLoginResult.ChallengeRequired)
+                    {
+                        var challenge = await api.GetChallengeRequireVerifyMethodAsync();
+                        if (challenge.Succeeded)
+                        {
+                            if (challenge.Value.SubmitPhoneRequired)
+                            {
+                                //Telefon ile işlem
+                            }
+                            else
+                            {
+                                if (challenge.Value.StepData != null)
+                                {
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.PhoneNumber))
+                                    {
+                                        var pnv = challenge.Value.StepData.PhoneNumber;
+                                        MessageBox.Show(pnv);
+                                        SendCode(api);
+                                    }
+                                    if (!string.IsNullOrEmpty(challenge.Value.StepData.Email))
+                                    {
+                                        var emv = challenge.Value.StepData.Email;
+                                        MessageBox.Show(emv);
+                                        SendCode(api);
+                                    }
+
+                                }
+                            }
+                        }
+                        else
+                            MessageBox.Show(challenge.Info.Message, "ERR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void SendCode(IInstaApi api)
+        {
+            bool isEmail = true;
+            try
+            {
+                if (isEmail)
+                {
+                    var email = await api.RequestVerifyCodeToEmailForChallengeRequireAsync();
+                    this.Size = new Size(429, 260);
+                    if (email.Succeeded)
+                    {
+                        MessageBox.Show("Gönderildi");
+                    }
+                    else
+                        MessageBox.Show(email.Info.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    var phoneNumber = await api.RequestVerifyCodeToSMSForChallengeRequireAsync();
+                    if (phoneNumber.Succeeded)
+                    {
+                        MessageBox.Show("Doğrulandı");
+                        this.Size = new Size(429, 200);
+                    }
+                    else
+                        MessageBox.Show(phoneNumber.Info.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+        }
+
+        public async void Dogrula(IInstaApi api)
+        {
+            try
+            {
+                var verifyLogin = await api.VerifyCodeForChallengeRequireAsync(txtDogrulama.Text);
+                if (verifyLogin.Succeeded)
+                {
+                    MessageBox.Show("Doğrulandı");
+                    this.Size = new Size(429, 200);
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Yanlış kod");
+                    if (verifyLogin.Value == InstaLoginResult.TwoFactorRequired)
+                    {
+                        MessageBox.Show("iki faktörlü doğrulama gerekiyor.");
+                    }
+                    else
+                        MessageBox.Show(verifyLogin.Info.Message, "ERR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "D");
+            }
+        }
+        int sayi = 0;
+
+        private async void Timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                sayi++;
+                if (sayi % 5 == 1)
+                {
+                    string dosyayolu = "resim.png";
+                    bmp = new Bitmap(dosyayolu);
+                    pbPP.Image = bmp;
+                    bmp.Save("resimpp.png");
+                    string ppyolu = "resimpp.png";
+                    byte[] bytes = File.ReadAllBytes(ppyolu);
+                    await api.AccountProcessor.ChangeProfilePictureAsync(bytes);
+                }
+                if (sayi % 5 == 0)
+                {
+                    string dosyaYolu = "resim1.png";
+                    bmp1 = new Bitmap(dosyaYolu);
+                    pbPP.Image = bmp1;
+                    bmp1.Save("resimPp.png");
+                    string ppYolu = "resimPp.png";
+                    byte[] Bytes = File.ReadAllBytes(ppYolu);
+                    await api.AccountProcessor.ChangeProfilePictureAsync(Bytes);
+
+                }
+                if (sayi % 3 == 0)
+                {
+                    string dosyayolu = "resim2.png";
+                    bmp2 = new Bitmap(dosyayolu);
+                    pbPP.Image = bmp2;
+                    bmp2.Save("resimPP.png");
+                    string ppyolu = "resimPP.png";
+                    byte[] bytes = File.ReadAllBytes(ppyolu);
+                    await api.AccountProcessor.ChangeProfilePictureAsync(bytes);
+                }
+                if (sayi % 3 == 1)
+                {
+                    string dosyayolu = "resim3.png";
+                    bmp3 = new Bitmap(dosyayolu);
+                    pbPP.Image = bmp3;
+                    bmp3.Save("ResimPP.png");
+                    string ppyolu = "ResimPP.png";
+                    byte[] bytes = File.ReadAllBytes(ppyolu);
+                    await api.AccountProcessor.ChangeProfilePictureAsync(bytes);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                this.Text = "Hata: " + ex.Message;
+            }
+        }
+    }
+}
